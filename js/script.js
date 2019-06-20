ed16e60cd136adb35aeff19f2daae6e683a9009b
endriu <aolaszewski@gmail.com>
2019-06-20 21:39:45 +0200

    fix toggle class features

--- js/script.js
+++ js/script.js
@@ -4,9 +4,14 @@ const loaders = document.querySelector('.loaders-wrapper');
 const pageText = document.querySelector('.loaded');
 
 const loadPage = () => {
-
   setInterval(() => {
-    loaders.classList.toggle('display');
+    if (!loaders.classList.contains('display')) {
+      loaders.classList.toggle('display');
+      pageText.classList.remove('display');
+    } else {
+      loaders.classList.remove('display');
+      pageText.classList.toggle('display');
+    }
   }, 3000);
 };
 
