d19ee22fcb376ab99cd6e75284934cae73906a9d
endriu <aolaszewski@gmail.com>
2019-06-20 22:12:40 +0200

    fix toggle features

--- js/script.js
+++ js/script.js
@@ -4,15 +4,14 @@ const loaders = document.querySelector('.loaders-wrapper');
 const pageText = document.querySelector('.loaded');
 
 const loadPage = () => {
-  setInterval(() => {
-    if (!loaders.classList.contains('display')) {
-      loaders.classList.toggle('display');
-      pageText.classList.remove('display');
-    } else {
-      loaders.classList.remove('display');
-      pageText.classList.toggle('display');
-    }
-  }, 3000);
+  if (!loaders.classList.contains('display')) {
+    loaders.classList.toggle('display');
+    pageText.classList.remove('display');
+  } else {
+    loaders.classList.remove('display');
+    pageText.classList.toggle('display');
+  }
 };
 
-loadPage();
+// setInterval(3000, loadPage());
+setInterval(function(){ loadPage(); }, 2000);
